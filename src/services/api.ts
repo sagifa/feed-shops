b3dfbae7c4d6dc196f7c4ad0b9f529852e9b7ae9
SagiGuyAvrahamCom <95570914+sagifa@users.noreply.github.com>
2023-02-06 12:13:44 +0200

    add observer

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,5 +7,11 @@ const api = {
       .then((res) => res)
       .catch((err) => err);
   },
+  postUserObserverFeed: async (userId: string, id: string) => {
+    return await axios
+      .post(`https://www.tedooo.com/?userId=${userId}&itemId=${id}`)
+      .then((res) => res)
+      .catch((err) => err);
+  },
 };
 export default api;
